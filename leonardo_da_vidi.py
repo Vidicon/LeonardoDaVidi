f33ebc6634545630b6ae781b829b8a9a5a2ebbb7
Bram Fenijn <bram.fenijn@nobleo.nl>
2023-03-17 17:01:48 +0100

    WIP

--- leonardo_da_vidi.py
+++ leonardo_da_vidi.py
@@ -12,15 +12,25 @@ import time
 class LeonardoDaVidi:
 
     def __init__(self):
-        self.gridmap = None
+        self.gridmap = np.zeros((1, 1), dtype=np.float64)
 
         self.enemie_kernel =  np.array([[1, 2, 1],
                               [2, 3, 2],
                               [1, 2, 1]])
         
-        self.enemie_kernel = self.gaussian_kernel((5,5), )
+        # self.enemie_kernel =  np.array([[1, 2, 3, 2, 1],
+        #                                 [2, 3, 4, 3, 2],
+        #                                 [3, 4, 5, 4, 3],
+        #                                 [2, 3, 4, 3, 2],
+        #                                 [1, 2, 3, 2, 1],])
+        
+        # self.enemie_kernel =  np.array([1])
+        
+        # self.enemie_kernel = self.gaussian_kernel((5,5))
         
         self.myid = 0
+
+        self.debug = True
 
         self.fig = None
         self.ax = None
@@ -90,10 +100,7 @@ class LeonardoDaVidi:
         j_max = min(j+2, self.gridmap.shape[1])
 
         sub_arr = self.gridmap[i_min:i_max, j_min:j_max]
-        print("sub_arr shape:", sub_arr.shape)
         result = self.enemie_kernel * multiplier
-        
-        print("result shape:", result.shape)
 
         if i_min == 0:
             result = result[1:,:]
@@ -125,18 +132,23 @@ class LeonardoDaVidi:
 
 
     def gridmap_update(self, grid, enemies):
-        for enemie in enemies:
-            print((enemie["position"][1],enemie["position"][0]))
-        print("")
+        if self.debug:
+            for enemie in enemies:
+                print((enemie["position"][1],enemie["position"][0]))
+            print("")
         for enemie in enemies:
             if enemie["id"] == self.myid:
                 continue
             pass
             self.add_kernel_to_gridmap((enemie["position"][1],enemie["position"][0]))
-            # self.put_kernel(enemie["position"], -1)
+
+        # for i in range(self.gridmap.shape[0]):
+        #     for j in range(self.gridmap.shape[1]):
+        #         self.gridmap[i][j] += self.can_overwrite(self.myid, grid[i][j])
+
         for i in range(self.gridmap.shape[0]):
             for j in range(self.gridmap.shape[1]):
-                self.gridmap[i][j] += self.can_overwrite(self.myid, grid[i][j])
+                self.gridmap[i][j] += min(self.gridmap[i][j], 255.0)
 
     # def can_overwrite(self, id, tile):
 
@@ -153,13 +165,13 @@ class LeonardoDaVidi:
     def display_gridmap(self):
         self.ax.clear()
         plt.ion()
-        self.ax.imshow(self.gridmap, cmap='gray', origin='lower')
+        self.ax.imshow(self.gridmap, cmap='inferno', origin='lower')
         plt.grid(True)
         # plt.xticks(range(27))
         # self.ax.colorbar()
         # print("gridmap: {}".format(self.gridmap.shape[0]))
         plt.show()
-        plt.pause(0.0001)
+        plt.pause(0.00001)
         # pprint(self.gridmap)
         
 
@@ -167,33 +179,52 @@ class LeonardoDaVidi:
         pos_x = self.position[0]
         pos_y = self.position[1]
         grid_size = grid.shape[0]
-        # print("Girdsize: {}".format(grid_size))
 
         self.myid = self.id
-
-        # if self.gridmap is not None:
-        #     self.ax.clear()
-        #     self.ax.imshow(self.gridmap)
-        #     plt.pause(0.001)
-        # else:
-        #     self.fig, self.ax = plt.subplots()
-        #     self.ax.imshow(self.gridmap)
-        #     plt.show()
-        # if self.gridmap is None:
-        self.gridmap = np.zeros((grid_size, grid_size), dtype=np.float64)
+        if self.gridmap.shape[0] == 1:
+            self.gridmap = np.zeros((grid_size, grid_size), dtype=np.float64)
         
         self.gridmap_update(grid, enemies)
 
-        # if self.heatmap is None:
-            # self.create_debug_window((grid_size, grid_size))
-            
-     
-            # self.draw_debug_window()
-        # self.heatmap.set_data(self.gridmap)
-        # self.fig.canvas.draw()
-        # self.draw_debug_window()
-        self.display_gridmap()
-
-
+        if self.debug:
+            self.display_gridmap()
 
         return Move.STAY
+    
+# ============================================================
+# The Final Times (Sorted to average)
+# ============================================================
+# Rank   Name                           Contributor          Avg [us]     Min [us]     Max [us]
+# 1    : RickbrandtVanRijn              Rick Voogt           0.0          0.0          0.0
+# 2    : Vector                         Ishu                 2.004        0.0          1003.504
+# 3    : Aslan                          Hakan                4.147        0.0          1001.358
+# 4    : ShortSpanDog                   Felipe               6.17         0.0          632.763
+# 5    : ch34tsRus                      Lewie                8.904        0.0          1002.312
+# 6    : The Clueless African           JP Potgieter         13.193       0.0          1013.756
+# 7    : Rambo The Rando                Nobleo               19.182       0.0          1006.842
+# 8    : Short Sighted Steve            Nobleo               34.968       0.0          1004.934
+# 9    : Greedy Gerard                  Rayman               48.528       0.0          1010.18
+# 10   : Big Ass Bot                    Mahmoud              55.416       0.0          2003.67
+# 11   : Picasso                        Daniel               96.585       0.0          1093.626
+# 12   : Atilla the Attacker            Jorik de Vries       288.048      0.0          4092.932
+# 13   : ID10+ BOT                      Nobleo               9729.869     0.0          38919.687
+# 14   : LeonardoDaVidi                 Bram Fenijn          230538.59    0.0          780239.82
+
+# ============================================================
+# The Final Times (Sorted to average)
+# ============================================================
+# Rank   Name                           Contributor          Avg [us]     Min [us]     Max [us]
+# 1    : RickbrandtVanRijn              Rick Voogt           0.005        0.0          5.245
+# 2    : Vector                         Ishu                 5.572        0.0          1001.12
+# 3    : ch34tsRus                      Lewie                8.936        0.0          1032.829
+# 4    : Aslan                          Hakan                12.21        0.0          1034.737
+# 5    : The Clueless African           JP Potgieter         15.834       0.0          1013.756
+# 6    : Rambo The Rando                Nobleo               19.041       0.0          1010.418
+# 7    : Greedy Gerard                  Rayman               22.425       0.0          1013.756
+# 8    : ShortSpanDog                   Felipe               23.061       0.0          1175.88
+# 9    : Short Sighted Steve            Nobleo               31.676       0.0          1007.557
+# 10   : Picasso                        Daniel               82.785       0.0          1339.197
+# 11   : Big Ass Bot                    Mahmoud              89.642       0.0          26062.727
+# 12   : Atilla the Attacker            Jorik de Vries       287.241      0.0          22000.79
+# 13   : LeonardoDaVidi                 Bram Fenijn          6085.807     0.0          41936.159
+# 14   : ID10+ BOT                      Nobleo               10042.317    0.0          52551.985
